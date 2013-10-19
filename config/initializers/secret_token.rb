cdc7117149594f6c58d76a4a885ff554997bcac3
Clelio De Souza <cleliofs@gmail.com>
2013-10-19 13:51:00 +0100

    Initial setup for Assessment Mate RoR project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AssessmentMate::Application.config.secret_key_base = 'd450e0b9550a31868dcb136cfa47972698f343b547cf7090d76c042f1fabe4136f8081c7f13f25c2dc3038adad00b36183beccc4968aa9f71fefdbd8fca1f3d7'
