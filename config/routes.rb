a7c414421c3636c6b0eb6128421cef18f98d4b4f
Clelio De Souza <cleliofs@gmail.com>
2013-10-20 21:36:07 +0100

    Adding questions index as root

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ AssessmentMate::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  #root 'welcome#index'
+  root 'questions#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
